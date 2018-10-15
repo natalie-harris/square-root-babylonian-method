399e8ce883c4a80ffaac7ffe2221462f0e12519f
tekops <32205174+tekops27@users.noreply.github.com>
2018-10-15 14:36:21 -0400

    Add files via upload

---
+++ squareroot.py
@@ -0,0 +1,37 @@
+#calculates square root of any real positive integer using the 'babylonian square root method'
+print("")
+print("This program calculates the square root of any real positive integer using the 'babylonian square root method'")
+
+#imports necessary modules
+import math
+
+#sets up variables
+isPosInt = False
+
+#while loop to ask for a positive integer to find the square root
+while isPosInt == False:
+	base = int(raw_input("What is the base number you want to find the square root of? "))
+	if str.isdigit(str(base)) == True:
+		isPosInt = True
+	else:
+		isPosInt = False
+		print("The base number must be a positive integer")
+		print("")
+	if base == 0 or base == 1:
+		isPosInt = False
+		print("The square root of " + str(base) + " is " + str(base))
+		print("")
+
+#find the guess of whatever the number is
+length = (len(str(base)))
+guess = int(math.ceil(base / (2 ** length)))
+guess = float(str(guess) + (".000"))
+
+#finds the square root of the base
+while guess != ((guess + (base / guess)) / 2):
+	guess = ((guess + (base / guess)) / 2)
+
+#prints the square root
+print("")
+print("The square root of " + str(base) + " is " + str(guess))
+print("")
